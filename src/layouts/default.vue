3530911c32e9d09edb7088ba2c5651de4685be8c
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2022-11-28 22:39:42 +0900

    [add] nuxt.jsの動作テスト

---
+++ src/layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <nav-ber />
+    <nuxt />
+  </div>
+</template>
---
+++ src/pages/detail.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>This is Detail Page</h1>
+        <h2>{{ subtitle }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  layout: "original",
+  data() {
+    return {
+      subtitle: 'この文書はNuxt.jsの基本的な使用方法について説明しています。'
+    }
+  }
+};
+</script>
